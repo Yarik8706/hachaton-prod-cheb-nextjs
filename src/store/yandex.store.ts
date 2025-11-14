49b130155ba216ee6f1614ddbd22af897a5b8334
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2025-11-14 16:42:04 +0300

    Create yandex.store.ts

---
+++ src/store/yandex.store.ts
@@ -0,0 +1,19 @@
+﻿import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { api } from "@/api/api";
+import {IProfile} from "@/store/types";
+
+
+interface ProfileStore {
+	loginUrl: string;
+	fetchLoginUrl: () => Promise<void>;
+}
+
+export const useYandexLogin = create<ProfileStore>()(
+	devtools((set) => ({
+		loginUrl: null,
+		fetchLoginUrl: async () => {
+			
+		},
+	}))
+);
