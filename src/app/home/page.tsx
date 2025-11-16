3299e6613550e6d21c157a358e583d49a390d47e
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2025-11-16 15:20:46 +0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -171,7 +171,7 @@ export default function HomePage() {
       search_text: "",
       tags: [],
       date: DateSearchPeriod.none,
-      sourceType: SourceType.All,
+      sourceType: "",
     } as SearchParams)
     handleFiltersSubmit()
   }
